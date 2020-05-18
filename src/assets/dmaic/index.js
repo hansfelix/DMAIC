6fcb87df9f50d0d9832b1d80cae32d7e076a5214
Hans Felix <hansfelix50@gmail.com>
2020-05-17 21:16:17 -0500

    move api key to env

--- src/assets/dmaic/index.js
+++ src/assets/dmaic/index.js
@@ -1,11 +1,12 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCjrPNmOdaBc8cQVjVJb1e2JkuSRLvXGhA",
-    authDomain: "dmaic-db196.firebaseapp.com",
-    databaseURL: "https://dmaic-db196.firebaseio.com",
-    projectId: "dmaic-db196",
-    storageBucket: "dmaic-db196.appspot.com",
-    messagingSenderId: "661466842340"
+    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_FIREBASE_APP_ID
 };
 firebase.initializeApp(config);
 
